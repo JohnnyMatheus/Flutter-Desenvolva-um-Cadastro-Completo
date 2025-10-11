ac4659e6b569ae1cc037eb389cfd53f4d8a7841b
Johnny Matheus <alunotioficial@gmail.com>
2025-10-11 01:47:49 -0300

    criado classe usuario

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Johnny',
+    email: 'johnny@teste.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2020/06/29/20/31/man-5354308_1280.png',
+  ),
+};
